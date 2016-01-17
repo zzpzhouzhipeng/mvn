bdd8779e149c86d3cd98d8c6c9e1f22c02079cda
chenliang <chenliang901114@sina.com>
2016-01-17 21:52:52 +0800

    maven test learn projects

---
+++ mavweb/src/main/java/com/chen/Test001.java
@@ -0,0 +1,7 @@
+package com.chen;
+
+public class Test001 {
+	public static void main(String[] args) {
+		System.out.println("wsd");
+	}
+}
---
+++ user-config/src/main/java/com/synnex/user_config/App.java
@@ -0,0 +1,13 @@
+package com.synnex.user_config;
+
+/**
+ * Hello world!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
