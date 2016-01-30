77972d5c3e7286b1b4a3651e1d4251223ebfec98
chenliang <chenliang901114@sina.com>
2016-01-30 23:30:59 +0800

    add wms porject

---
+++ wms/wms-dao/src/main/java/com/synnex/wms/dao/App.java
@@ -0,0 +1,13 @@
+package com.synnex.wms.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ wms/wms-service/src/main/java/com/synnex/wms/service/App.java
@@ -0,0 +1,13 @@
+package com.synnex.wms.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
