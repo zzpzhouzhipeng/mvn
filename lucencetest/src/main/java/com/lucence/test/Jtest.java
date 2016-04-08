c5c1f8edd15f56e9dd22f684002712ee137ae537
chenliang <chenliang901114@sina.com>
2016-04-08 13:56:20 +0800

    update lucencetest

---
+++ lucencetest/src/main/java/com/lucence/test/Jtest.java
@@ -0,0 +1,9 @@
+package com.lucence.test;
+
+public class Jtest {
+	public static void main(String[] args) {
+		for(int i=0;i<args.length;i++) {
+			System.out.println("args["+i+"]="+args[i]);
+		}
+	}
+}
